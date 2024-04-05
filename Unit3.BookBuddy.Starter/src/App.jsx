923580afea0c3596da06ff62051b4f3874539f23
lucydelash <lucyrudd614@gmail.com>
2024-04-05 17:39:16 -0400

    added new icon

--- Unit3.BookBuddy.Starter/src/App.jsx
+++ Unit3.BookBuddy.Starter/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
   return (
     <>
       <h1>
-        <img src="https://e7.pngegg.com/pngimages/465/30/png-clipart-paper-clip-computer-icons-free-content-free-parchment-background-white-shoe.png" alt='Quill Logo' />
+        <img src="" alt='' />
         Library App
       </h1>
 
